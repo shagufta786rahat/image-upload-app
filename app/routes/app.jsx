2e8cf1a34c25960af416b45014118497d3be9ca2
abhi9-jais <abhi9jaiswal45@gmail.com>
2026-08-13 17:37:09 +0530

    change

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -16,9 +16,9 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        {/* <s-link href="/app/customers">Home</s-link> */}
+        {/* <s-link href="/app">Home</s-link> */}
         <s-link href="/app/customers">Customer List</s-link>
-        {/* <s-link href="/app/additional">Additional page</s-link> */}
+        <s-link href="/app/wishlist">Wishlist</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
